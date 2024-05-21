5c2f3f070c4e0eb0f418aee68dcb7e31fafdeaf9
Juan <ramirezjuan9806@gmail.com>
2024-05-21 00:17:06 -0500

    Se agrega scikit-learn a requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,20 +1,4 @@
-""" from fastapi import FastAPI, File, UploadFile
-import fitz  # PyMuPDF
-import docx
-from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
-from fastapi.middleware.cors import CORSMiddleware
-from langdetect import detect, DetectorFactory
-from langdetect.lang_detect_exception import LangDetectException
-import re
-from models.profile import Profile
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
-import seaborn as sns
-import datetime as dt
-import warnings
-import plotly.graph_objects as go """
+
 from fastapi import FastAPI
 from app.api.api import api_router
 from app.core.cors import configure_cors
